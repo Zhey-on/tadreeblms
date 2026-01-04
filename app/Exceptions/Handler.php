d36bf5a16456e88c7ac127f735b68e49322c601e
anupdeveloper <anupdeveloper07@gmail.com>
2026-01-04 16:29:57 +0530

    fixing

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,52 +3,47 @@
 namespace App\Exceptions;
 
 use Throwable;
+use ErrorException;
 use Illuminate\Auth\AuthenticationException;
 use Spatie\Permission\Exceptions\UnauthorizedException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 
-/**
- * Class Handler.
- */
 class Handler extends ExceptionHandler
 {
-    /**
-     * A list of the exception types that are not reported.
-     *
-     * @var array
-     */
     protected $dontReport = [
         //
     ];
 
-    /**
-     * A list of the inputs that are never flashed for validation exceptions.
-     *
-     * @var array
-     */
     protected $dontFlash = [
         'password',
         'password_confirmation',
     ];
 
-    /**
-     * Report or log an exception.
-     *
-     * @param  \Exception  $exception
-     * @return void
-     */
     public function report(Throwable $exception)
     {
         parent::report($exception);
     }
 
     /**
-     * Render an exception into an HTTP response.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Exception  $exception
-     * @return \Illuminate\Http\Response
+     * Register exception handling callbacks.
      */
+    public function register()
+    {
+        $this->renderable(function (ErrorException $e, $request) {
+            if (str_contains($e->getMessage(), 'Attempt to read property')) {
+
+                if ($request->expectsJson()) {
+                    return response()->json([
+                        'status' => false,
+                        'message' => 'Requested data not found'
+                    ], 404);
+                }
+
+                abort(404, 'Requested data not found');
+            }
+        });
+    }
+
     public function render($request, Throwable $exception)
     {
         if ($exception instanceof UnauthorizedException) {
@@ -60,12 +55,6 @@ class Handler extends ExceptionHandler
         return parent::render($request, $exception);
     }
 
-    /**
-     * @param \Illuminate\Http\Request $request
-     * @param AuthenticationException  $exception
-     *
-     * @return \Illuminate\Http\JsonResponse|\Illuminate\Http\RedirectResponse
-     */
     protected function unauthenticated($request, AuthenticationException $exception)
     {
         return $request->expectsJson()
